b6858e55d951bedde3bfa194d57728a8f84a872f
d4n11083 <d4n11083@estudiantec.cr>
2018-10-16 16:52:30 -0600

    Clase mapa creada

---
+++ SDL_Window_Test/Mapa.cpp
@@ -0,0 +1,9 @@
+//
+//  Mapa.cpp
+//  SDL_Window_Test
+//
+//  Created by Josue Chaves on 10/16/18.
+//  Copyright © 2018 Josue Chaves. All rights reserved.
+//
+
+#include "Mapa.hpp"
---
+++ SDL_Window_Test/Mapa.h
@@ -0,0 +1,14 @@
+//
+//  Mapa.hpp
+//  SDL_Window_Test
+//
+//  Created by Josue Chaves on 10/16/18.
+//  Copyright © 2018 Josue Chaves. All rights reserved.
+//
+
+#ifndef Mapa_hpp
+#define Mapa_hpp
+
+#include <stdio.h>
+
+#endif /* Mapa_hpp */
